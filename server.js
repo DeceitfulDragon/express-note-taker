cc2db04576e83cf303e1aab5dd3235a231dd3ace
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-04-26 03:24:40 -0500

    html routes

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const express = require("express");
 const database = require("./db/database");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+// Retrieving notes
 app.get("/api/notes", (req, res) => {
-
-    database.getNotes().then((notes) => {
-        return res.status(200).json(notes); // status to 200 on success
-    }).catch((err) => res.status(500).json(err)); // status to 500 on error
+    database.getNotes()
+        .then(notes => res.status(200).json(notes))
+        .catch(err => res.status(500).json({ error: err.message }));
 });
 
+// Adding notes
 app.post("/api/notes", (req, res) => {
     const note = req.body;
 
-    database.addNotes(note).then(addedNote => { 
-        res.status(200).json(addedNote) 
-    }).catch(err => res.status(400).json({ err }));
+    database.addNotes(note)
+        .then(addedNote => res.status(200).json(addedNote))
+        .catch(err => res.status(400).json({ error: err.message }));
+});
+
+// Deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const { id } = req.params;
+
+    database.deleteNotes(id)
+        .then(() => res.status(200).json({ deleted: true }))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// For HTML Routes
+// notes.html
+app.get("/notes", (req, res) => {
+    res.sendfile(path.join(__dirname, "public/notes.html"));
+});
+
+// index.html
+app.get("*", (req, res) => {
+    res.sendfile(path.join(__dirname, "public/index.html"));
 });
 
 // Log that the server is running
