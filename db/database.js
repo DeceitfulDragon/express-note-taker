9cbe7584e45ded436af68846f549d18df7763bac
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-04-26 02:17:39 -0500

    Working Database class, logs notes

--- db/database.js
+++ db/database.js
@@ -13,29 +13,46 @@ class Database {
     }
 
     // Write note file
-    write() {
+    write(note) {
         return writeFile("db/db.json", JSON.stringify(note));
     }
 
     // Get notes
     getNotes() {
         return this.read().then((notes) => {
-            let parsedNotes;
+            let parsedNotes = [];
 
-            try { parsedNotes.push(JSON.parse(notes)); }
+            try { parsedNotes = JSON.parse(notes); }
             catch(err) { parsedNotes = []; }
             return parsedNotes;
-
-        })
+        });
     }
 
     // Add notes
-    addNotes() {
-
+    addNotes(note) {
+        const { title, text } = note;
+        
+        // Make sure the note has a title and text
+        if (!title || !text) { throw new Error("Please provide a title and text content for the note!"); }
+    
+        const noteWithId = { title, text, id: uniqid() };
+    
+        return this.getNotes()
+            .then(notes => {
+                const updatedNotes = [...notes, noteWithId];
+                return this.write(updatedNotes);
+            })
+            .then(() => noteWithId);  // Returns new note with ID
     }
 
     // Delete notes
-    deleteNotes() {
-
+    deleteNotes(id) {
+        return this.getNotes().then((notes) => {
+            const filteredNotes = notes.filter(note => note.id !== id);
+            return this.write(filteredNotes);
+        });
     }
 }
+
+// Log notes
+new Database().getNotes().then((notes) => console.log(notes));
--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("./api/notes", (req, res) => {
+
+})
+
 // Log that the server is running
 app.listen(PORT, () => { console.log(`Server started using port: ${PORT}`) })
