968eb73147d4da28cad385c14267b3fff7bf8845
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-04-26 01:39:50 -0500

    create database.js for handling read / write

---
+++ db/save.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const utl = require("util");
+var uniqid = require('uniqid');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+class Save {
+
+    // Read note file
+    read() {
+        return readFile("db/db.json", "utf8");
+    }
+
+    // Write note file
+    write() {
+        return this.write("db/db.json", JSON.stringify(note));
+    }
+
+    // Get notes
+    getNotes() {
+
+    }
+
+    // Add notes
+    addNotes() {
+
+    }
+
+    // Delete notes
+    deleteNotes() {
+
+    }
+}
